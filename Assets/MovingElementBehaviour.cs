2ce9bc32a351f6fb52cc135813e4008a0d9ef03e
Daniel González Fajardo <danielgonzalezfajardo@MBP-de-Daniel.home>
2021-03-09 17:36:43 +0000

    Attempt for autonomous navmesh

---
+++ Assets/MovingElementBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovingElementBehaviour : MonoBehaviour
+{
+       
+    void Start () {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = new Vector3(22.09f,transform.position.y,24.68f); 
+        Debug.Log("moving");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
